096ca63f7cb0bbb0288802c986ac566e3bb44ec7
Technical hub <62637097+technicalhub9@users.noreply.github.com>
2020-04-01 19:42:09 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+public class Anagram
+{
+ static void isAnagram(String s1, String s2)
+ 	{
+		boolean status = true;
+		if (s1.length() != s2.length()) 
+		{
+			status = false;
+                }
+		else 
+			{
+			char[] ArrayS1 = s1.toLowerCase().toCharArray();
+			char[] ArrayS2 = s2.toLowerCase().toCharArray();
+			Arrays.sort(ArrayS1);
+			Arrays.sort(ArrayS2);
+			status = Arrays.equals(ArrayS1, ArrayS2);
+		        }
+		if (status) 
+		{
+			System.out.println(s1 + " and " + s2 + " are anagrams");
+		}
+ 		else 
+		{
+			System.out.println(s1 + " and " + s2 + " are not anagrams");
+		}
+	}
+ 
+	public static void main(String[] args) {
+		isAnagram("peek", "Peek");
+		isAnagram("Mother In Law", "Hitler Woman");
+	}
+}
